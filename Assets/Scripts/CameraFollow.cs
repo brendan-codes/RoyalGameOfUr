72adc87f5e561fdf994b055912edd2170fa606ab
Brendan Stanton <bmsenate@gmail.com>
2019-02-04 10:59:57 -0800

    added camera and game state scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public float cameraSize = 0.5f;
+
+    public Camera thisCam;
+    public Vector3 offset;
+
+
+    void LateUpdate(){ // like update, but later
+        transform.position = target.position + offset;
+        // thisCam.orthographicSize = thisCam.orthographicSize + cameraSize;
+    }
+
+
+    // camera follow script
+
+    // - alerted when movement is happening
+    // - accepts target moving tile
+    // - updates orthographicSize to see the board and 'zoom out'
+    // - resets to original location
+
+}
---
+++ Assets/gameStateController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameStateController : MonoBehaviour
+{
+
+    // gameStateController
+    // things to track:
+    // - isMoving: is an animation active?
+    // - currentPlayer: who's turn is it?
+    // - hasWinner: is the game over, has a player won?
+    // - isRolling: is the rolling animation played?
+    // - selectedTile: what's the tile that's being moved?
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
